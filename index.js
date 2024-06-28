ccedcaf8a472c1a894b366b061f11fecc4cfd77b
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-06-28 13:03:10 -0400

    Add pseudocode

---
+++ index.js
@@ -0,0 +1,6 @@
+// Create message for user about lock code.
+
+/* Assign three variables representing parts of the lock code. 
+Each variable is a calculation using a unique arithmetic operator.*/
+
+// Create a popup dialog box displaying the vault codes and the text.
