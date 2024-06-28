4c48d1b1aaed33794f3746e06c0a7ba27ff50e71
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-06-28 13:16:06 -0400

    Write string for user about lock code.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Create message for user about lock code.
+`You have received this message because you have been chosen to open an important vault. 
+Here is the secret combination:`
 
 /* Assign three variables representing parts of the lock code. 
 Each variable is a calculation using a unique arithmetic operator.*/
